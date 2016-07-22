019a7419b48b8c142649186caa656de1ef7a4615
rishi2893 <rishi2893@gmail.com>
2016-07-22 12:20:04 +0530

    added buttons using css

--- Trello/MyTrello/admin.py
+++ Trello/MyTrello/admin.py
@@ -3,6 +3,23 @@ from django.contrib import admin
 
 from .models import Board, Card, List
 
-admin.site.register(Board)
-admin.site.register(Card)
-admin.site.register(List)
+class ListInline(admin.StackedInline):
+    model = List
+    extra = 2
+
+class CardInline(admin.StackedInline):
+    model = Card
+    extra = 2
+
+class CardAdmin(admin.ModelAdmin):
+    inlines = [ListInline]
+
+class BoardAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['board_name']}),
+        ('Date information', {'fields': ['create_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [CardInline]
+
+admin.site.register(Board, BoardAdmin)
+admin.site.register(Card, CardAdmin)
